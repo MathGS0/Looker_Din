20e9833238291427bd642d36432b1fbad7cd3c11
MathGS0 <matheusgregoriosilva.02@gmail.com>
2024-06-03 10:59:54 -0300

    Update viz.js

--- viz.js
+++ viz.js
@@ -1,19 +1,43 @@
 const dscc = require('@google/dscc');
-const local = require('./localMessage.js');
 
-// Verifica se estamos em desenvolvimento local
-export const LOCAL = false;
+const drawViz = (data) => {
+  // Limpa o gráfico existente
+  document.body.innerHTML = '';
 
-function drawViz(data) {
-  // Lógica para desenhar a visualização
-  const div = document.createElement('div');
-  div.textContent = JSON.stringify(data);
-  document.body.appendChild(div);
-}
+  // Extrai dados
+  const dim1 = data.tables.DEFAULT[0].dim1;
+  const metric1 = data.tables.DEFAULT[0].metric1;
+  const metric2 = data.tables.DEFAULT[0].metric2;
+  const metric3 = data.tables.DEFAULT[0].metric3;
 
-// Chama a função drawViz com os dados apropriados
-if (LOCAL) {
-  drawViz(local.message);
-} else {
-  dscc.subscribeToData(drawViz, {transform: dscc.objectTransform});
-}
+  // Criação do gráfico customizado (Exemplo básico)
+  const container = document.createElement('div');
+  container.style.width = '100%';
+  container.style.height = '100%';
+
+  const chartTitle = document.createElement('h2');
+  chartTitle.innerText = 'Título do Gráfico';
+  container.appendChild(chartTitle);
+
+  // Adiciona colunas
+  const column1 = document.createElement('div');
+  column1.style.height = `${metric1}px`;
+  column1.style.width = '50px';
+  column1.style.backgroundColor = 'orange';
+  container.appendChild(column1);
+
+  const column2 = document.createElement('div');
+  column2.style.height = `${metric2}px`;
+  column2.style.width = '50px';
+  column2.style.backgroundColor = 'blue';
+  container.appendChild(column2);
+
+  const diffText = document.createElement('div');
+  diffText.innerText = `Diferença: ${metric3}`;
+  container.appendChild(diffText);
+
+  document.body.appendChild(container);
+};
+
+// Subscrição para atualização dos dados
+dscc.subscribeToData(drawViz, {transform: dscc.objectTransform});
